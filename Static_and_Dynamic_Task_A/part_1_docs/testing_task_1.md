e6bac65645e6c778cfdf3031882980ac1016b0f4
Mark Richardson <mark_richardson@sky.com>
2020-12-03 14:55:52 +0000

    comments added for part 1, test file set up for part 2

--- Static_and_Dynamic_Task_A/part_1_docs/testing_task_1.md
+++ Static_and_Dynamic_Task_A/part_1_docs/testing_task_1.md
@@ -16,14 +16,15 @@ Only comment on errors that would stop the tests running.
 
 class CardGame:
 
-
+# this function has an error when checking the value of the card. should have double equals ==, not single =. 
+# The else statement should also have a colon after it, as such... else:
   def check_for_ace(self, card):
     if card.value = 1:
       return True
     else
       return False
    
-
+# this function has a spelling error when defining the function. should read def, not dif
   dif highest_card(self, card1 card2):
   if card1.value > card2.value:
     return card
@@ -31,7 +32,7 @@ class CardGame:
     return card2
   
 
-
+# this function has an error when defining the initial value of 'total. should be total = "" to give it an initial empty/0 value
 def cards_total(self, cards):
   total
   for card in cards:
--- Static_and_Dynamic_Task_A/part_2_code/specs/card_game_tests.py
+++ Static_and_Dynamic_Task_A/part_2_code/specs/card_game_tests.py
@@ -2,5 +2,33 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        self.card1 = Card(clubs, 7)
+        self.card2 = Card(spades, 5)
+        self.card3 = Card(diamonds, 3)
+        self.card4 = Card(hearts, 1)
+
+
+@unittest.skip("Delete this line to run the test")
+    def test_card_has_suit():
+        pass
+
+
+@unittest.skip("Delete this line to run the test")
+    def card_has_value():
+        pass
+
+
+@unittest.skip("Delete this line to run the test")
+    def test_check_for_ace():
+        pass
+
+@unittest.skip("Delete this line to run the test")
+    def test_highest_card():
+        pass
+
+
+@unittest.skip("Delete this line to run the test")
+    def test_cards_total():
+        pass
